b2672f5b408210acd61074cf6659b281aed88f63
Vrushali Vishal Patil <vrushali@Vrushalis-MacBook-Air.local>
2023-04-07 19:33:32 -0400

    env file added

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
+require('dotenv').config(); // loads environment variables from .env file
 const express = require('express');
+
 const app = express();
+
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.send('Hi Rusu Hello World!');
 });
 
-app.listen(9000, () => {
-  console.log('Hi Rusu Server listening on port 9000');
+app.listen(PORT, () => {
+  console.log(`Hi Rusu Server listening on port, ${PORT}` );
 });
